5fe5e0da4088bebe72134ace77e25d3e9d27cd5d
dionysus0214 <hiba0214@naver.com>
2022-04-15 15:06:20 +0900

    mui 및 관련 icon 설치 및 버튼 샘플 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>wasakbasak</p>
+        <Button variant="contained">
+          Hello World
+        </Button>
       </header>
     </div>
   );
