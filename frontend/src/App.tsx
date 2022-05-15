a5843e1e745130ceb09ecd2298a3c3a47b351e14
dionysus0214 <hiba0214@naver.com>
2022-05-15 19:36:59 +0900

    mui 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
@@ -8,16 +9,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          WasakbasaK
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          variant="contained"
+          onClick={() => {
+            alert('here');
+          }}
         >
-          Learn React
-        </a>
+          GO WASAK
+        </Button>
       </header>
     </div>
   );
