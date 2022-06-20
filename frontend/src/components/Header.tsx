22fb7f794819e2e31b4267770442972499a77e9a
eunjin0212 <eunjin9639@gmail.com>
2022-06-20 22:39:29 +0900

    header style 수정

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,8 +14,9 @@ const Container = styled.div`
 	font-size: 20px;
 	font-weight: 700;
   height: 56px;
-  width: 100vw;
+  width: 1680px;
   justify-content: space-between;
   display: flex;
   align-items: center;
+  margin: 0 auto;
 `;
