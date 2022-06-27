5b6bc4bd3f13fab6de71eb08614aeb27de2e7ea8
eunjin0212 <eunjin9639@gmail.com>
2022-06-28 00:15:02 +0900

    Amend Ranking style

--- frontend/src/components/Ranking.tsx
+++ frontend/src/components/Ranking.tsx
@@ -5,10 +5,13 @@ import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
+import { createTheme } from '@mui/material/styles';
 import Tab from '@mui/material/Tab';
-import Item from './Item';
-
 import { useState } from 'react';
+import Item from './Item';
+const themes = createTheme({
+	spacing: 5,
+});
 const useStyles = makeStyles((theme) => ({
 	list: {
 		width: '100%',
@@ -38,6 +41,9 @@ const useStyles = makeStyles((theme) => ({
 		alignItems: 'center',
 		// marginBottom: '20px',
 	},
+  spacing: {
+    padding: themes.spacing(4),
+  }
 }));
 
 export default function Ranking() {
@@ -60,7 +66,7 @@ export default function Ranking() {
 				text: 'Do you have Paris recommendations? Have you ever…',
 			},
 		],
-    floor5: [
+		floor5: [
 			{
 				img: '/static/images/avatar/1.jpg',
 				name: 'floor5',
@@ -77,7 +83,7 @@ export default function Ranking() {
 				text: 'Do you have Paris recommendations? Have you ever…',
 			},
 		],
-    floor11: [
+		floor11: [
 			{
 				img: '/static/images/avatar/1.jpg',
 				name: 'floor11',
@@ -103,56 +109,58 @@ export default function Ranking() {
 	};
 	return (
 		<TabContext value={tabValue}>
-			<Box
-				sx={{
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-				}}>
-				<div className={classes.title}>
-					Ranking
-					<TabList
-						value={tabValue}
-						onChange={handleChange}
-						variant='scrollable'>
-						<Tab label='snack' value='snack' />
-						<Tab label='drink' value='drink' />
-					</TabList>
-				</div>
-				<Button variant='text' size='small' className={classes.paddingNone}>
-					View all
-				</Button>
-			</Box>
-			<TabPanel value='snack' sx={{ padding: 0, display: 'flex' }}>
-				{Object.values(floorsRankingData).map((floor, index) => (
-					<List className={classes.list} disablePadding={true} key={index}>
-						{floor.map((items, idx) => (
-							<Item
-								name={items.name}
-								img={items.img}
-								text={items.text}
-								badge={{ display: true, content: idx + 1}}
-								key={idx}
-							/>
-						))}
-					</List>
-				))}
-			</TabPanel>
-			<TabPanel value='drink' sx={{ padding: 0, display: 'flex' }}>
-				{Object.values(floorsRankingData).map((floor, idx) => (
-					<List className={classes.list} disablePadding={true} key={idx}>
-						{floor.map((items, idx) => (
-							<Item
-								name={items.name}
-								img={items.img}
-								text={items.text}
-								badge={{ display: false }}
-								key={idx}
-							/>
-						))}
-					</List>
-				))}
-			</TabPanel>
+			<div className={classes.spacing}>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}>
+					<div className={classes.title}>
+						Ranking
+						<TabList
+							value={tabValue}
+							onChange={handleChange}
+							variant='scrollable'>
+							<Tab label='snack' value='snack' />
+							<Tab label='drink' value='drink' />
+						</TabList>
+					</div>
+					<Button variant='text' size='small' className={classes.paddingNone}>
+						View all
+					</Button>
+				</Box>
+				<TabPanel value='snack' sx={{ padding: 0, display: 'flex' }}>
+					{Object.values(floorsRankingData).map((floor, index) => (
+						<List className={classes.list} disablePadding={true} key={index}>
+							{floor.map((items, idx) => (
+								<Item
+									name={items.name}
+									img={items.img}
+									text={items.text}
+									badge={{ display: true, content: idx + 1 }}
+									key={idx}
+								/>
+							))}
+						</List>
+					))}
+				</TabPanel>
+				<TabPanel value='drink' sx={{ padding: 0, display: 'flex' }}>
+					{Object.values(floorsRankingData).map((floor, idx) => (
+						<List className={classes.list} disablePadding={true} key={idx}>
+							{floor.map((items, idx) => (
+								<Item
+									name={items.name}
+									img={items.img}
+									text={items.text}
+									badge={{ display: false }}
+									key={idx}
+								/>
+							))}
+						</List>
+					))}
+				</TabPanel>
+			</div>
 		</TabContext>
 	);
 }
