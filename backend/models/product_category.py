30345247f9d6a72b5e77410c293e14b686bfc2b6
kuy <kuy472013@gmail.com>
2022-06-29 00:54:28 +0900

    모델 수정

---
+++ backend/models/product_category.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from db.base_class import Base
+from sqlalchemy.orm import relationship
+
+
+class ProductCategory(Base):
+    __tablename__ = "product_categories"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), unique=True, index=True, nullable=False)
+
+    products = relationship("Product", back_populates="product_category")
