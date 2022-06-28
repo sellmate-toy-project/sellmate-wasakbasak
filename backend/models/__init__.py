d98cfdfe44297841e071daed55d1b765d4aefc06
kuy <kuy472013@gmail.com>
2022-06-29 00:53:59 +0900

    모델 관련 설정 수정

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -6,4 +6,5 @@ from .review_like import ReviewLike
 from .order import Order
 from .post import Post
 from .post_comment import PostComment
-from .review_comment import  ReviewComment
+from .review_comment import ReviewComment
+from .product_category import ProductCategory
