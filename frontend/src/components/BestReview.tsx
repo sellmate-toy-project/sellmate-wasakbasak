4d6b979d46a5cd7b718c74da96726bd37eb58134
devstar1224 <dev_sang@naver.com>
2022-06-28 20:19:55 +0900

    탭 생성

--- frontend/src/components/BestReview.tsx
+++ frontend/src/components/BestReview.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 import FlipCard from "./FlipCard";
 import React from "react";
-import List from "@mui/material/List";
-import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import TabList from "@mui/lab/TabList";
 import Tab from "@mui/material/Tab";
 import Button from "@material-ui/core/Button";
+import {TabPanel} from "@mui/lab";
+import {Tabs, Typography} from "@mui/material";
 
 const BestReview = () => {
     const CardData = [
@@ -33,20 +32,69 @@ const BestReview = () => {
         },
     ];
 
+    interface TabPanelProps {
+        children?: React.ReactNode;
+        index: number;
+        value: number;
+    }
+
+    function TabPanel(props: TabPanelProps) {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box sx={{ p: 3 }}>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
+    function a11yProps(index: number) {
+        return {
+            id: `simple-tab-${index}`,
+            'aria-controls': `simple-tabpanel-${index}`,
+        };
+    }
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
     return (
         <BestReviewWrapper>
-            {
-                CardData.map((data, index) => (
-                        <FlipCard
-                            frontLeftImage={data.leftImage}
-                            frontRightImage={data.rightImage}
-                            backContentText={data.backText}
-                            frontContentText={data.frontText}
-                            backImage={data.backImage}
-                        />
+            <Box sx={{ width: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Snack" {...a11yProps(0)} />
+                        <Tab label="Drink" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                {
+                    CardData.map((data, index) => (
+                            <FlipCard
+                                frontLeftImage={data.leftImage}
+                                frontRightImage={data.rightImage}
+                                backContentText={data.backText}
+                                frontContentText={data.frontText}
+                                backImage={data.backImage}
+                            />
+                        )
                     )
-                )
-            }
+                }
+                </TabPanel>
+            </Box>
         </BestReviewWrapper>
     );
 };
