cfc51dc2a5c6ae8f873aee39c6caaea451ae7cc2
ohMySunset <znfnznfnakqj@naver.com>
2022-06-28 23:43:43 +0900

    refactor(post) : 모델 수정
    - 컬럼 추가

--- backend/models/post.py
+++ backend/models/post.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Boolean
 from sqlalchemy.sql.functions import func
 from sqlalchemy.schema import ForeignKey
 from db.base_class import Base
@@ -9,8 +9,10 @@ class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String(200), nullable=False)
     body = Column(String(1000), nullable=True)
     created_at = Column(TIMESTAMP, server_default=func.now())
-
+    updated_at = Column(TIMESTAMP, onupdate=func.now())
+    is_Deleted = Column(Boolean(), nullable=True, default=False)
     user = relationship("User", back_populates="posts")
     post_comments = relationship("PostComment", back_populates="post")
