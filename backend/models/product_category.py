6ae97f079e26289fd339e716a37e0981a470d1be
kuy <kuy472013@gmail.com>
2022-07-11 22:08:25 +0900

    product category table 수정

--- backend/models/product_category.py
+++ backend/models/product_category.py
@@ -7,5 +7,6 @@ class ProductCategory(Base):
     __tablename__ = "product_categories"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), unique=True, index=True, nullable=False)
+    owner_id = Column(Integer)
 
     products = relationship("Product", back_populates="product_category")
