262b5e7dd4ca460db4ceb72a732b8774ebcc359f
ohMySunset <znfnznfnakqj@naver.com>
2022-07-17 20:35:05 +0900

    fix: 라우터 수정

--- backend/routes/api.py
+++ backend/routes/api.py
@@ -4,5 +4,5 @@ from controllers import *
 api_router = APIRouter()
 api_router.include_router(user_controller.router, prefix="/users", tags=["users"])
 api_router.include_router(collect_controller.router, prefix="/collect", tags=["collect"])
-api_router.include_router(post_controller.router, prefix="/post", tags=['post'])
+api_router.include_router(post_controller.router, prefix="/posts", tags=['posts'])
 api_router.include_router(product_category_controller.router, prefix="/product/categories", tags=['product'])
