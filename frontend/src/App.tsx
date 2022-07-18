a2d7a25ab2c287eec716199ce8f78e0eb52b791b
eunjin0212 <eunjin9639@gmail.com>
2022-07-18 22:44:22 +0900

    react-router 추가, login page 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,42 @@
-import styled from "styled-components";
-import Header from "./components/Header";
-import LeftContainer from "./components/LeftContainer";
-import Main from "./components/Main";
-import RightContainer from "./components/RightContainer";
-
+import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from './components/Header';
+import LeftContainer from './components/LeftContainer';
+import Login from './components/Login';
+import Main from './components/Main';
+import RightContainer from './components/RightContainer';
 const App = () => {
-    return (
-        <DashBoard className='App'>
-            <Header/>
-            <Werapper>
-                <LeftContainer/>
-                <Main/>
-                <RightContainer/>
-            </Werapper>
-        </DashBoard>
-    );
+	return (
+		<div className='App'>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<Home>
+							<Header />
+							<Werapper>
+								<LeftContainer />
+								<Main />
+								<RightContainer />
+							</Werapper>
+						</Home>
+					}
+				/>
+				<Route path='login' element={<Login />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
-const DashBoard = styled.div`
+const Home = styled.div`
 	text-align: center;
-  font-family: Pretendard;
+	font-family: Pretendard;
 `;
 const Werapper = styled.div`
 	margin: 0 auto;
 	width: 1680px;
-  padding: 22px 0 40px 0;
-  display: flex;
-  justify-contant: center
+	padding: 22px 0 40px 0;
+	display: flex;
+	justify-contant: center;
 `;
---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,16 @@
+// import { makeStyles } from '@material-ui/core/styles';
+
+
+// const useStyles = makeStyles((theme) => ({
+// }));
+
+const Login = () => {
+	// const classes = useStyles();
+
+	return (
+			<div>
+        login
+      </div>
+	);
+};
+export default Login;
