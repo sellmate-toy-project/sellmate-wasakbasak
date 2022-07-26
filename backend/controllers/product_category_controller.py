bcf3a3db3609673a7d25c15e9475ca1cf07cecdd
kuy <kuy472013@gmail.com>
2022-07-27 00:09:00 +0900

    상품 카테고리 조회 API 수정

--- backend/controllers/product_category_controller.py
+++ backend/controllers/product_category_controller.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Optional
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from . import deps
@@ -13,7 +13,7 @@ def read_product_categories(
     db: Session = Depends(deps.get_db),
     skip: int = 0,
     limit: int = 100,
-    owner_id: int = None,
+    owner_id: Optional[int] = None,
 ) -> Any:
     if owner_id is not None:
         product_categories = crud.product_category.get_multi_by_owner(db, owner_id=owner_id, skip=skip, limit=limit)
