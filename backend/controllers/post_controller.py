213834002b2a732e38ed0a6542902c33b0e6bf9c
ohMySunset <znfnznfnakqj@naver.com>
2022-08-07 23:24:24 +0900

    fix: post crud 수정
    - 리턴값 수정
    - crud 로직 수정

--- backend/controllers/post_controller.py
+++ backend/controllers/post_controller.py
@@ -1,12 +1,14 @@
-from typing import Any, List
+from controllers.response_entity import ResponseEntity
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from starlette.requests import Request
+from typing import Any, List
 from . import deps
-import schemas
+
 import crud
+import schemas
 
 router = APIRouter()
-
 
 @router.get("/", response_model=List[schemas.Post])
 def read_items(
@@ -27,27 +29,34 @@ def read_item(
     return post
 
 
-@router.post("/", response_model=schemas.Post)
+@router.post("/", response_model=ResponseEntity)
 def create_item(
+    request: Request,
     data: schemas.PostCreate,
     db: Session = Depends(deps.get_db),
 ) -> Any:
-    post = crud.post.create(db, data)
-    return post
+    obj = data.dict(exclude_unset=True)
+    post = crud.post.create(db, obj)
+    return ResponseEntity(httpMethod=request.method, path=request.url.path, body=post)
 
 
-@router.put("/{post_id}", response_model=schemas.Post)
+@router.put("/{post_id}", response_model=ResponseEntity)
 def update(
+    request: Request,
     post_id: int,
     data: schemas.PostUpdate,
     db: Session = Depends(deps.get_db),
 ) -> Any:
-    post = crud.post.update(db, post_id, data)
-    return post
+    obj = data.dict(exclude_unset=True)
+    post = crud.post.update(db, post_id, obj)
+    return ResponseEntity(httpMethod=request.method, path=request.url.path, body=post)
 
-@router.delete("/{post_id}", response_model=schemas.Post)
+
+@router.delete("/{post_id}", response_model=ResponseEntity)
 def delete(
+    request: Request,
+    post_id: int,
     db: Session = Depends(deps.get_db),
-    post_id: int = Path(...)
 ) -> Any:
     post = crud.post.delete(db, post_id)
+    return ResponseEntity(httpMethod=request.method, path=request.url.path, body=post)
--- backend/crud/crud_post.py
+++ backend/crud/crud_post.py
@@ -1,45 +1,33 @@
 from typing import Optional
 from .base import CRUDBase
 from models.post import Post
-from schemas.post_schema import PostCreate
-from schemas.post_schema import PostUpdate
-from sqlalchemy.orm import Session, Query
+from sqlalchemy.orm import Session
 
+import schemas
 
 class CRUDPost(CRUDBase[Post, None, None]):
-    def create(self, db: Session, obj_in: PostCreate) -> Post:
+    def create(self, db: Session, obj_in: schemas.PostCreate) -> Post:
         db_obj = self.model(**obj_in)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
         return db_obj
 
-    def update(self, db: Session, post_id, obj_in: PostUpdate) -> Post:
-        post = (
-            Query(self.model).
-                with_session(db).
-                filter(Post.id == post_id).
-                filter(Post.is_deleted == 0).
-                first()
-        )
-
-        post.title = obj_in.title
-        post.body = obj_in.body
+    def update(self, db: Session, post_id, obj_in: schemas.PostUpdate) -> Post:
+        db_obj = self.model(**obj_in)
+        db.query(self.model)\
+            .filter(Post.id == post_id)\
+            .filter(Post.is_deleted == 0)\
+            .update({'body': obj_in['body'], 'title': obj_in['title']})
 
         db.flush()
         db.commit()
-        return post
+        return db_obj
 
     def delete(self, db: Session, post_id: int) -> Post:
-        post = (
-            Query(self.model).
-                with_session(db).
-                filter(Post.id == post_id).
-                filter(Post.is_deleted == 0).
-                first()
-        )
-
-        post.is_deleted = 1
+        post = db.query(self.model)\
+        .filter(Post.id == post_id)\
+        .update({'is_deleted': 1})
 
         db.flush()
         db.commit()
