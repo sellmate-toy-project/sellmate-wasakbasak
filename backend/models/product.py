7e9c1ecf84fdad6570e5950397507fa0156908bd
kuy <kuy472013@gmail.com>
2022-08-10 00:05:41 +0900

    상품 테이블 code 컬럼 추가

--- backend/models/product.py
+++ backend/models/product.py
@@ -16,6 +16,7 @@ class Product(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     product_category_id = Column(Integer, ForeignKey("product_categories.id"))
     name = Column(String(100), unique=True, index=True, nullable=False)
+    code = Column(String(100), unique=True, index=True, nullable=False)
     desc = Column(String(1000))
     price = Column(Integer, nullable=False)
     status = Column(Enum(StatusType), nullable=True, server_default="active")
@@ -24,4 +25,3 @@ class Product(Base):
     product_likes = relationship("ProductLike", back_populates="product")
     reviews = relationship("Review", back_populates="product")
     product_category = relationship("ProductCategory", back_populates="products")
-
