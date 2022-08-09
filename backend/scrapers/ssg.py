2d6b550a890e1a2743ba3a505515a50e3182dbf9
kuy <kuy472013@gmail.com>
2022-08-09 23:56:37 +0900

    ssg 상품 수집

--- backend/scrapers/ssg.py
+++ backend/scrapers/ssg.py
@@ -1,6 +1,7 @@
 import requests
 import crud
 from bs4 import BeautifulSoup
+import time
 
 
 class Ssg:
@@ -42,6 +43,13 @@ class Ssg:
                             for sub_category in sub_categories:
                                 sub_product_category = self.insert_category(product_category, sub_category)
                                 self.sub_product_categories.append(sub_product_category)
+                        else:
+                            raise Exception(f"ssg sub category request error: {sub_res}")
+
+                        # SSG 스크래핑 연속적인 새로고침 오류 방지를 위해 3초 대기
+                        time.sleep(5)
+                else:
+                    raise Exception(f"ssg middle category request error: {res}")
             except Exception as e:
                 print(f'Server Error: {e}')
 
@@ -66,22 +74,41 @@ class Ssg:
     def collect_product(self):
         for sub_product_category in self.sub_product_categories:
             try:
-                res = requests.get(self.base_uri + sub_product_category.get('code') + "&sort=regdt&pageSize=100")
+                res = requests.get(self.base_uri + sub_product_category.code + "&sort=regdt&pageSize=100")
                 if res.status_code == 200:
                     # 상품 리스트 파싱
                     soup = BeautifulSoup(res.text, "html.parser")
                     products = soup.select(".cunit_thmb_lst li")
 
                     for product in products:
-                        crud.product.create(
-                            db=self.db,
-                            obj_in={
-                                "name": product.select(".cunit_info .tx_ko")[0].text,
-                                "desc": product.select(".thmb img")[0]['src'],
-                                "price": product.select(".cunit_info .ssg_price")[0].text.replace(",", "")
-                            },
-                            product_category_id=sub_product_category.get('id')
-                        )
-                        raise
+                        product_dict = {
+                            "name": product.select(".cunit_info .tx_ko")[0].text,
+                            "code": product.select(".thmb a")[0]['data-info'],
+                            "desc": product.select(".thmb img")[0]['src'],
+                            "price": product.select(".cunit_info .ssg_price")[0].text.replace(",", "")
+                        }
+                        self.insert_product(product_dict, sub_product_category.id)
+
+                    # SSG 스크래핑 연속적인 새로고침 오류 방지를 위해 3초 대기
+                    time.sleep(5)
+                else:
+                    raise Exception(f"ssg product request error: {res}, "
+                                    f"category code : {sub_product_category.code}")
             except Exception as e:
                 print(f'Server Error: {e}')
+
+    def insert_product(self, product_dict, product_category_id):
+        product = crud.product.duplicate_check(
+            self.db,
+            name=product_dict.get("name"),
+            code=product_dict.get("code")
+        )
+
+        if not product:
+            product = crud.product.create(
+                db=self.db,
+                obj_in=product_dict,
+                product_category_id=product_category_id
+            )
+
+        return product
