116d5090858ff6b9e60b57e90779e5f05a9655b7
kuy <kuy472013@gmail.com>
2022-08-15 22:16:37 +0900

    product 테이블 수정

--- backend/models/product.py
+++ backend/models/product.py
@@ -1,5 +1,6 @@
 import enum
-from sqlalchemy import Column, Integer, String, Enum, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Enum, UniqueConstraint, TIMESTAMP
+from sqlalchemy.sql.functions import func
 from db.base_class import Base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
@@ -14,15 +15,17 @@ class StatusType(enum.Enum):
 class Product(Base):
     __tablename__ = "products"
     __table_args__ = (
-        UniqueConstraint('name', 'code', name='uix_name_code'),
+        UniqueConstraint('product_category_id', 'code', name='uix_name_code'),
     )
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     product_category_id = Column(Integer, ForeignKey("product_categories.id"))
-    name = Column(String(100), index=True, nullable=False)
+    name = Column(String(1000), nullable=False)
     code = Column(String(100), index=True, nullable=False)
     desc = Column(String(1000))
     price = Column(Integer, nullable=False)
     status = Column(Enum(StatusType), nullable=True, server_default="active")
+    created_at = Column(TIMESTAMP, server_default=func.now())
+    updated_at = Column(TIMESTAMP, server_default=func.now())
 
     orders = relationship("Order", back_populates="product")
     product_likes = relationship("ProductLike", back_populates="product")
