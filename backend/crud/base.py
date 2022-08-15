c40b145631fa83a814aac8c1d8c33b5eb6677374
kuy <kuy472013@gmail.com>
2022-08-15 22:16:16 +0900

    base crud 정렬 추가

--- backend/crud/base.py
+++ backend/crud/base.py
@@ -2,10 +2,16 @@ from typing import Generic, TypeVar, Type, Optional, Any, List
 from db.base_class import Base
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+import enum
 
 ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+
+
+class SortType(enum.Enum):
+    DESC = "desc"
+    ASC = "asc"
 
 
 class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
@@ -15,5 +21,12 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     def get(self, db: Session, id: Any) -> Optional[ModelType]:
         return db.query(self.model).filter(self.model.id == id).first()
 
-    def get_multi(self, db: Session, skip: int = 0, limit: int = 100) -> List[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()
+    def get_multi(
+        self,
+        db: Session,
+        skip: int = 0,
+        limit: int = 100,
+        sort: str = "id",
+        sort_by: SortType = "asc"
+    ) -> List[ModelType]:
+        return db.query(self.model).order_by(f"{sort} {sort_by.value}").offset(skip).limit(limit).all()
