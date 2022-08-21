8be926991bc659cbf3665c391ddddcc49bb34617
ohMySunset <znfnznfnakqj@naver.com>
2022-08-21 13:41:37 +0900

    fix: read post 수정

--- backend/crud/crud_post.py
+++ backend/crud/crud_post.py
@@ -2,10 +2,26 @@ from typing import Optional
 from .base import CRUDBase
 from models.post import Post
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 
 import schemas
 
-class CRUDPost(CRUDBase[Post, None, None]):
+
+class CRUDPost(CRUDBase[Post, None, None, None]):
+    def get_posts(self, db: Session, skip, limit, sort, sort_by, user_id) -> Post:
+        posts = post.get_multi(db, skip, limit, sort, sort_by)
+
+        if user_id:
+            posts = db.query(self.model)\
+                .filter(Post.user_id == user_id)\
+                .order_by(text(f"{sort} {sort_by}"))\
+                .offset(skip)\
+                .limit(limit)\
+                .all()
+            return posts
+
+        return posts
+
     def create(self, db: Session, obj_in: schemas.PostCreate) -> Post:
         db_obj = self.model(**obj_in)
         db.add(db_obj)
