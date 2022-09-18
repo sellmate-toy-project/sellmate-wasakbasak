067230da6b003a4b9d568fa10266b6b0ffaea165
kuy <kuy472013@gmail.com>
2022-09-18 10:58:49 +0900

    오류 응답 핸들링 수정

--- backend/controllers/response_entity.py
+++ backend/controllers/response_entity.py
@@ -6,20 +6,23 @@ from pydantic.generics import GenericModel
 T = TypeVar("T")
 
 
-class PagingMeta(BaseModel):
-    total_page: int
-    current_page: Union[int, None]
-    size: Union[int, None]
+class Response(BaseModel):
+    httpStatus: int = 200
+    httpMethod: Any
+    message: str = ""
+    path: Any
+    timestamp: str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M%S")
 
 
 class Body(GenericModel, Generic[T]):
     body: Sequence[T]
 
 
-class ResponseEntity(Body, Generic[T]):
-    httpStatus: int = 200
-    httpMethod: Any
-    message: str = ""
-    path: Any
-    timestamp: str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M%S")
+class PagingMeta(BaseModel):
+    total_page: int
+    current_page: Union[int, None]
+    size: Union[int, None]
+
+
+class ResponseEntity(Response, Body, Generic[T]):
     paging_meta: Optional[PagingMeta]
