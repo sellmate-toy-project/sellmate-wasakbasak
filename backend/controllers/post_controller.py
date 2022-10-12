066bbdf5039f8ad493a166cd88d5f34dae01d147
ohMySunset <znfnznfnakqj@naver.com>
2022-10-12 22:37:32 +0900

    refactor: 응답구조 변경 및 예외처리 추가

--- backend/controllers/post_controller.py
+++ backend/controllers/post_controller.py
@@ -1,9 +1,11 @@
 from controllers.response_entity import ResponseEntity
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette.requests import Request
-from typing import Any, List
+from typing import Any
 from . import deps
+from fastapi_pagination import Params
+from fastapi_pagination.ext.sqlalchemy import paginate
 
 import crud
 import schemas
@@ -11,29 +13,49 @@ import schemas
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=ResponseEntity[schemas.Post])
 def read_posts(
+    request: Request,
+    params: Params = Depends(),
     db: Session = Depends(deps.get_db),
-    skip: int = 0,
-    limit: int = 100,
     sort: str = "id",
     sort_by: crud.SortType = crud.SortType.ASC,
-    user_id: int = None
+    user_id: int = None,
 ) -> Any:
-    posts = crud.post.get_posts(db, skip, limit, sort, sort_by, user_id)
-    return posts
+    query = crud.post.get_posts(db, sort, sort_by, user_id)
+    posts = paginate(query, params)
+
+    return ResponseEntity(
+        httpMethod=request.method,
+        path=request.url.path,
+        body=posts.items,
+        paging_meta={
+            'total_page': round(query.count() / params.size),
+            'current_page': params.page,
+            'size': params.size,
+        },
+    )
 
 
-@router.get("/{post_id}", response_model=schemas.Post)
+@router.get("/{post_id}", response_model=ResponseEntity[schemas.Post])
 def read_post(
+    request: Request,
     post_id: int,
     db: Session = Depends(deps.get_db),
 ) -> Any:
     post = crud.post.get(db, id=post_id)
-    return post
+
+    if not post:
+        raise HTTPException(400, '존재 하지 않는 게시글 입니다.')
+
+    return ResponseEntity(
+        httpMethod=request.method,
+        path=request.url.path,
+        body=[post],
+    )
 
 
-@router.post("/", response_model=ResponseEntity)
+@router.post("/", response_model=ResponseEntity[schemas.Post])
 def create_item(
     request: Request,
     data: schemas.PostCreate,
@@ -41,10 +63,14 @@ def create_item(
 ) -> Any:
     obj = data.dict()
     post = crud.post.create(db, obj)
-    return ResponseEntity(httpMethod=request.method, path=request.url.path, body=post)
+    return ResponseEntity(
+        httpMethod=request.method,
+        path=request.url.path,
+        body=[post],
+    )
 
 
-@router.put("/{post_id}", response_model=ResponseEntity)
+@router.put("/{post_id}", response_model=ResponseEntity[schemas.Post])
 def update(
     request: Request,
     post_id: int,
@@ -53,14 +79,22 @@ def update(
 ) -> Any:
     obj = data.dict()
     post = crud.post.update(db, post_id, obj)
-    return ResponseEntity(httpMethod=request.method, path=request.url.path, body=post)
+    return ResponseEntity(
+        httpMethod=request.method,
+        path=request.url.path,
+        body=[post]
+    )
 
 
-@router.delete("/{post_id}", response_model=ResponseEntity)
+@router.delete("/{post_id}", response_model=ResponseEntity[schemas.Post])
 def delete(
     request: Request,
     post_id: int,
     db: Session = Depends(deps.get_db),
 ) -> Any:
     post = crud.post.delete(db, post_id)
-    return ResponseEntity(httpMethod=request.method, path=request.url.path, body=post)
+    return ResponseEntity(
+        httpMethod=request.method,
+        path=request.url.path,
+        body=[post]
+    )
