b8313971fb3326fd48def010b953b017bd16d98f
kuy <kuy472013@gmail.com>
2022-10-24 20:42:51 +0900

    상품 카테고리 기본 값 수정

--- backend/migrations/versions/85abf1eb5fb1_product_category_bulk_insert.py
+++ backend/migrations/versions/85abf1eb5fb1_product_category_bulk_insert.py
@@ -8,7 +8,7 @@ Create Date: 2022-07-12 11:09:35.596744
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.sql import table, column
-from sqlalchemy import String
+from sqlalchemy import String, Integer
 
 # revision identifiers, used by Alembic.
 revision = '85abf1eb5fb1'
@@ -17,7 +17,8 @@ branch_labels = None
 depends_on = None
 
 product_categories_table = table('product_categories',
-                                 column('name', String)
+                                    column('name', String),
+                                    column('owner_id', Integer)
                                  )
 
 
@@ -25,8 +26,8 @@ def upgrade() -> None:
     op.bulk_insert(
         product_categories_table,
         [
-            {'name': '과자'},
-            {'name': '음료'},
+            {'name': '과자', 'owner_id': 0},
+            {'name': '음료', 'owner_id': 0},
         ]
     )
 
